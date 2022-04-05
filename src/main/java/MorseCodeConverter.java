9f0c94bb17102ffb26a5702018e3ae47513f1e24
SaraYaffe <syaffe@student.touro.edu>
2022-04-04 23:04:45 -0400

    test methods and ignored incompatible characters in conversion methods

--- src/main/java/MorseCodeConverter.java
+++ src/main/java/MorseCodeConverter.java
@@ -1,43 +1,48 @@
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MorseCodeConverter {
 
     private static final String[] code
-            = { ".-",   "-...", "-.-.", "-..",  ".",
-            "..-.", "--.",  "....", "..",   ".---",
-            "-.-",  ".-..", "--",   "-.",   "---",
-            ".--.", "--.-", ".-.",  "...",  "-",
-            "..-",  "...-", ".--",  "-..-", "-.--",
-            "--.." };
+            = {".-", "-...", "-.-.", "-..", ".",
+            "..-.", "--.", "....", "..", ".---",
+            "-.-", ".-..", "--", "-.", "---",
+            ".--.", "--.-", ".-.", "...", "-",
+            "..-", "...-", ".--", "-..-", "-.--",
+            "--.."};
     private final Map<String, Character> morseToEnglish = new HashMap<>();
 
-    public MorseCodeConverter(Map<String, Character> morseToEnglish){
+    public MorseCodeConverter() {
         for (int i = 0; i < 26; i++) {
-            morseToEnglish.put(code[i], (char)('a' + i));
+            morseToEnglish.put(code[i], (char) ('a' + i));
         }
 
     }
 
-    public String toMorseCode(String englishInput){
+    public String toMorseCode(String englishInput) {
         StringBuilder morseString = new StringBuilder();
         String[] array = englishInput.split(" ");
+
         for (String s : array) {
             for (int i = 0; i < s.length(); i++) {
-                morseString.append(code[englishInput.charAt(i) - 'a']);
+                try {
+                    morseString.append(code[s.charAt(i) - 'a']).append(" ");
+                } catch (Exception ignored) {}
             }
-            morseString.append(" ");
-
         }
 
         return morseString.toString();
     }
 
-    public String toEnglish(String morseCodeInput){
+    public String toEnglish(String morseCodeInput) {
         StringBuilder englishString = new StringBuilder();
         String[] array = morseCodeInput.split(" ");
+
         for (String s : array) {
-            englishString.append(morseToEnglish.get(s));
+            if(morseToEnglish.get(s) != null){
+                englishString.append(morseToEnglish.get(s));
+            }
         }
 
         return englishString.toString();
--- src/test/java/MorseCodeConverterTest.java
+++ src/test/java/MorseCodeConverterTest.java
@@ -7,19 +7,48 @@ class MorseCodeConverterTest {
     @Test
     public void toMorseCode(){
         //given
+        MorseCodeConverter converter = new MorseCodeConverter();
 
         //when
 
         //then
+        assertEquals("-- --- .-. ... . -.-. --- -.. . ", converter.toMorseCode("morsecode"));
+        assertEquals("-- --- .-. ... . -.-. --- -.. . ", converter.toMorseCode("morse code"));
+    }
+
+    @Test
+    public void toMorseCode_notCompatible(){
+        //given
+        MorseCodeConverter converter = new MorseCodeConverter();
+
+        //when
+
+        //then
+        assertEquals("-- --- .-. ... . -.-. --- -.. . ", converter.toMorseCode("morse012code"));
+
     }
 
     @Test
     public void toEnglish(){
         //given
+        MorseCodeConverter converter = new MorseCodeConverter();
 
         //when
 
         //then
+        assertEquals("morsecode", converter.toEnglish("-- --- .-. ... . -.-. --- -.. ."));
+    }
+
+    @Test
+    public void toEnglish_notCompatible(){
+        //given
+        MorseCodeConverter converter = new MorseCodeConverter();
+
+        //when
+
+        //then
+        assertEquals("morsecode", converter.toEnglish("-- jk --- .-. ... / . -.-. --- -.. ."));
+
     }
 
 }
